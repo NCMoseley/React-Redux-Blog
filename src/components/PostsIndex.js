17cca983a39d42136603af803aeab7bea9c84139
NCMoseley <ncmoseley@gmail.com>
2018-06-29 14:33:28 -0700

    Build out Postsindex component

--- src/components/PostsIndex.js
+++ src/components/PostsIndex.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions";
+import _ from "lodash";
 
 class PostsIndex extends Component {
   // It dosen't matter if the data fetching is done before or after the component is called, React is asyncronously getting the data asap.
@@ -8,13 +9,32 @@ class PostsIndex extends Component {
   componentDidMount() {
     this.props.fetchPosts();
   }
+
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li key={post.id} className="list-group-item">
+          {post.title}
+        </li>
+      );
+    });
+  }
   render() {
-    return <div>I SPIT HOT FIRE</div>;
+    return (
+      <div>
+        <h3>posts</h3>
+        <ul className="list-group">{this.renderPosts()}</ul>
+      </div>
+    );
   }
 }
 
-// fetchPosts here is equal to { fetchPosts: fetchPosts }, and used in this manner, without map dispatch to props gives this component access to the redux store, with a slightly different syntax.
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
+
+// fetchPosts here is equal to { fetchPosts: fetchPosts }, and used in this manner, without mapDispatchToProps still gives this component access to the redux store, with a slightly different syntax.
 export default connect(
-  null,
+  mapStateToProps,
   { fetchPosts }
 )(PostsIndex);
